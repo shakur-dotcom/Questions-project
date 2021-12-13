803c5db33c1bb3e208d23816788140052d8fc37e
shakur-dotcom <94641765+shakur-dotcom@users.noreply.github.com>
2021-12-13 21:45:49 +0000

    added logic to index.html
    The buttons in index.html can now expand and collapse when clicked upon.

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
 //using selectors inside the element
 // traversing the dom
+
+const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach(function (btn) {
+//   btn.addEventListener("click", function (e) {
+//     let question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle("show-text");
+//   });
+// });
+
+const questions = document.querySelectorAll('.question');
+console.log(questions);
+
+questions.forEach(function(question) {
+    let btn = question.querySelector('.question-btn');
+    btn.addEventListener('click', function() {
+        questions.forEach(function(item) {
+            if (item !== question) {
+                item.classList.remove('show-text');
+            }
+        })
+        question.classList.toggle('show-text');
+    })
+})
